f75bee082710866fb88fa9c3845f9624d8f5b595
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-09-22 10:24:56 -0300

    [FEATURE]teste para upvote e downvote

--- back-end/tests/integration/postRecommendations.test.ts
+++ back-end/tests/integration/postRecommendations.test.ts
@@ -1,11 +1,18 @@
-import * as recomandationFactory from "./factories/recommendationFactory";
+import * as recommendationDataFactory from "./factories/recommendationFactory";
 import { prisma } from "../../src/database";
 import supertest from "supertest";
 import app from "../../src/app";
 
+beforeEach(async () => {
+  await prisma.$executeRaw`TRUNCATE TABLE recommendations`;
+});
+afterAll(() => {
+  prisma.$disconnect();
+});
+
 describe("Test  POST /reomendations", () => {
   it("Create a new recomendation", async () => {
-    const recommendationData = recomandationFactory.recommendationData();
+    const recommendationData = recommendationDataFactory.recommendationData();
 
     const result = await supertest(app)
       .post("/recommendations")
@@ -18,6 +25,34 @@ describe("Test  POST /reomendations", () => {
     expect(createdRecommendation).not.toBe(null);
   });
 
-  it.todo("Upvote in recomendation");
-  it.todo("Dowvote in recomendation");
+  it("Upvote in recomendation", async () => {
+    const recommendationData = recommendationDataFactory.recommendationData();
+
+    await supertest(app).post("/recommendations").send(recommendationData);
+
+    const createdRecommendation = await prisma.recommendation.findUnique({
+      where: { name: recommendationData.name },
+    });
+
+    const result = await supertest(app).post(
+      `/recommendations/${createdRecommendation.id}/upvote`
+    );
+
+    expect(result.status).toBe(200);
+  });
+  it("Downvote in recomendation", async () => {
+    const recommendationData = recommendationDataFactory.recommendationData();
+
+    await supertest(app).post("/recommendations").send(recommendationData);
+
+    const createdRecommendation = await prisma.recommendation.findUnique({
+      where: { name: recommendationData.name },
+    });
+
+    const result = await supertest(app).post(
+      `/recommendations/${createdRecommendation.id}/downvote`
+    );
+
+    expect(result.status).toBe(200);
+  });
 });
