b16b4669fbaab5aaf5cf5fbab87c0ac3a10f9317
Luis Henrique <luishenrique@Luiss-MacBook-Air.local>
2022-09-22 09:17:47 -0300

    [FEATURE]criando e organizando arquivos de testes de integracao

---
+++ back-end/tests/integration/factories/recomendationFactory.ts
@@ -0,0 +1,8 @@
+import { faker } from "@faker-js/faker";
+
+export function recomendationData() {
+  const data = {
+    name: faker.lorem.words(2),
+    youtubeLink: "https://www.youtube.com/watch?v=rUxyKA_-grg",
+  };
+}
